1bbe3f7a7ba082bbb593cb67f164315a77b9cc1e
Gutovp1 <paivaav@gmail.com>
2022-04-21 18:16:27 -0300

    Complete first test reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    //text = 'augusto';
+    let textAux = text;
+    text= "";
+    for (i=0;i<=textAux.length;i++){
+        text += textAux.charAt(textAux.length-i);
+    }
+    return text;
+    
 };
 
 // Do not edit below this line
