94dfb888434382eed6b22592878b9ba33e787225
Gutovp1 <paivaav@gmail.com>
2022-04-22 11:29:20 -0300

    Create the function and pass the first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayArg, elements) {
+    if(arrayArg.includes(elements))
+        arrayArg.splice(arrayArg.indexOf(elements,1),1);   
+    return arrayArg;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
