c639421d10ccdce59b1eba2939b6452ae9b5e5dc
Gutovp1 <paivaav@gmail.com>
2022-04-22 12:26:22 -0300

    Update the function and pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,15 @@
-const removeFromArray = function(arrayArg, elements) {
-    if(arrayArg.includes(elements))
-        arrayArg.splice(arrayArg.indexOf(elements,1),1);   
-    return arrayArg;
+const removeFromArray = function(arrayArg, ...args) {
+    let elements =   Array.from(args);
+    for (i=0;i<=elements.length;i++)
+    {
+        if(arrayArg.includes(elements[i]))
+            arrayArg.splice(arrayArg.indexOf(elements[i],0),1);
+    }
+    // elements.forEach(()=>{
+    //  if(arrayArg.includes(elements))
+    //         arrayArg.splice(arrayArg.indexOf(elements,0),1);
+    // )};
+     return arrayArg;
 };
 
 
