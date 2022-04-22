7b77477b909875553820d6096ff8b130a617f37a
Gutovp1 <paivaav@gmail.com>
2022-04-22 15:06:52 -0300

    Create leap year check and all tests passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let leapDay = new Date(year,1,29);
+    if (leapDay.getMonth()==1)
+        return true;
+    else
+        return false;
 };
 
 // Do not edit below this line
