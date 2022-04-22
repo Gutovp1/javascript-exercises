20f2a2e263e4be7da79b5156e6c44d9b16896462
Gutovp1 <paivaav@gmail.com>
2022-04-22 13:01:45 -0300

    Update the function and pass all the tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,11 @@
 const sumAll = function(num1, num2) {
     let sum = 0;
+    if (num1<0 || num2<0 || typeof(num1) != 'number' || typeof(num2) != 'number')
+        return 'ERROR';
     for (i=num1;i<=num2;i++)
         sum +=i;
+    for (i=num2;i<=num1;i++)
+        sum +=i;    
     return sum;
 
 };
