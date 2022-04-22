600f0f50ee1ac2afd24d956b95c2e316098100e0
Gutovp1 <paivaav@gmail.com>
2022-04-22 15:29:32 -0300

    Create F to C and C to F functions and pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(Fah) {
+  let Cel = Math.round(((Fah-32)*5/9*10))/10;
+  return Cel;
 };
 
-const ctof = function() {
-
+const ctof = function(Cel) {
+  let Fah = Math.round((Cel*9/5+32)*10)/10;
+  return Fah;
 };
 
 // Do not edit below this line
