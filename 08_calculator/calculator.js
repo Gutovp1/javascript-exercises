cd23731dd81d83e2a469c113985b8adf5b85c9c2
Gutovp1 <paivaav@gmail.com>
2022-05-05 11:12:36 -0300

    implement the calculator and test it

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
+const add = function(a,b) {
+  return a+b;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arrays) {
+  if (!arrays.length)
+    return 0;
+  const total = arrays.reduce((pSum, arr) =>(pSum + arr),0);    
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arrays) {
+  if (!arrays.length)
+    return 0;
+  const result = arrays.reduce((pProd, arr) => pProd*arr,1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return Math.pow(a,b);	
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result=1;
+  if(a==0)
+    return 1;
+  else{
+    for (i=0;i<a;i++){
+      result = result*(a-i); 
+    }
+  return result;
+  }
 };
 
 // Do not edit below this line
