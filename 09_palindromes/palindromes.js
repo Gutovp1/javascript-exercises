b8317e57d85c01fb162655e289e3e18567c04a67
Gutovp1 <paivaav@gmail.com>
2022-05-05 12:24:28 -0300

    detect palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str1) {
+    let str2 = str1.toLowerCase();
+    str2=str2.replace(/!/g,"");// /g replaces all the matches
+    str2=str2.replace(/,/g,"");
+    str2=str2.replace(/\./g,"");
+    str2=str2.replace(/ /g,"");
+    let length = str2.length;
+    for(i=length-1;i>=0;i--)
+    {
+        if (str2.charAt(length-1-i) !== str2.charAt(i) )
+            return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
