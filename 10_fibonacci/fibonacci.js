e1a5b3149d794e8dd4ec6a1370bc297155ab5064
Gutovp1 <paivaav@gmail.com>
2022-05-05 21:42:46 -0300

    Create fibo sequence and test it

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if(index <= 0)
+        return 'OOPS';
+    let fiboSum = new Array(index);
+    fiboSum[0] = 1;
+    fiboSum[1] = 1;
+    for(i=2;i<index;i++)
+    {    
+        fiboSum[i] = fiboSum[i-1]+fiboSum[i-2];
+    }
+    return fiboSum[index-1];
 };
 
 // Do not edit below this line
