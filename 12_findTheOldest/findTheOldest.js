502fc61c11228e35ff46a0b0dd701bb863739db9
Gutovp1 <paivaav@gmail.com>
2022-05-06 12:34:00 -0300

    Find oldest sorting array of objs

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = (arrayObj) => {
+    let today = new Date();
+    today = today.getFullYear();
+    const ordered = arrayObj.sort((a,b) => {
+        if(!a.yearOfDeath)
+            a.yearOfDeath = today;
+        const older = a.yearOfDeath - a.yearOfBirth;
+        if(!b.yearOfDeath)
+            b.yearOfDeath = today;
+        const younger = b.yearOfDeath - b.yearOfBirth;       
+        return older > younger ? -1 : 1;
+    });
+    console.table(ordered);
+    return ordered[0];
 };
 
 // Do not edit below this line
